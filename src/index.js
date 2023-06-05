572c99ce578edfd7bb8e4a1f9df0f8e6dcc3637c
Mariano Santos <marianoveronsantos@gmail.com>
2023-06-05 00:28:32 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -48,8 +48,13 @@ const imageStorage = multer.diskStorage({
 const uploadImage = multer({ storage: imageStorage }).single("image");
 const upload = multer({ storage: storage });
 
-
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permitir cualquier origen
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(connect(mysql, dbConfig, "single"));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
