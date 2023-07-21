51671d20fbd76c25837a278dbea97ed7ebea2800
Mariano Santos <marianoveronsantos@gmail.com>
2023-07-20 22:08:25 -0300

    ads

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,84 +1,100 @@
 // Audience
-exports.addAudiencia = (req, res) => {
+exports.editAudience = (req, res) => {
   req.getConnection((err, connect) => {
     if (err) return res.send(err);
 
-    const data = {
+    const dataToBeChanged = {
       dni: req.body.dni,
       name: req.body.name,
-      lastname: req.body.lastname,
-      status: req.body.status,
-      email: req.body.email,
-      emailSyngenta: req.body.emailSyngenta,
-      dob: new Date(req.body.dob),
-      phone: req.body.phone,
-      address: req.body.address,
-      address2: req.body.address2,
-      location: req.body.location,
-      zipCode: req.body.zipCode,
-      province: req.body.province,
-      cuil: req.body.cuil,
       area: req.body.area,
-      ingress: new Date(req.body.ingress),
-      importation: "New Hires Mayo",
+      status: req.body.status,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      phone: req.body.phone,
+      importation: req.body.importation,
       added: new Date(req.body.added),
-      emailsSent: 0,
-      imageURL1: req.body.imageURL1 || null,
-      imagelURL2: req.body.imagelURL2 || null,
+      ingress: new Date(req.body.ingress),
+      dob: new Date(req.body.dob),
     };
+
+    // Check if imageURL1 needs to be updated
+    if (req.files && req.files.length > 0) {
+      const imageURL1 = req.protocol + "://" + req.get("host") + "/uploads/img/" + req.files[0].filename;
+      dataToBeChanged.imageURL1 = imageURL1;
+    }
+
+    // Check if imageURL2 needs to be updated
+    if (req.files && req.files.length > 1) {
+      const imageURL2 = req.protocol + "://" + req.get("host") + "/uploads/img/" + req.files[1].filename;
+      dataToBeChanged.imageURL2 = imageURL2;
+    }
 
     // Iniciar transacción
     connect.beginTransaction((err) => {
       if (err) return res.send(err);
 
-      // Insertar en la tabla de audiencia
-      connect.query("INSERT INTO audiencia SET ?", [data], (err, result) => {
-        if (err) {
-          connect.rollback(() => {
-            res.send(err);
-          });
-        } else {
-          const registroId = result.insertId;
-          const accionId = 1; // Valor de la acción inicial (ejemplo: 1 para "Creó")
+      // Obtener el registro actual
+      connect.query(
+        "SELECT * FROM audiencia WHERE id = ?",
+        [req.params.id],
+        (err, currentRecord) => {
+          if (err) {
+            connect.rollback(() => {
+              res.send(err);
+            });
+          } else {
+            const registroId = req.params.id;
+            const accionId = 2; // Valor de la acción para la edición (ejemplo: 2 para "Editó")
 
-          // Insertar en la tabla de registros_acciones
-          const nuevaAccion = {
-            accionId,
-            registroId,
-            fecha: new Date(),
-          };
+            // Insertar en la tabla de registros_acciones
+            const nuevaAccion = {
+              accionId,
+              registroId,
+              fecha: new Date(),
+            };
 
-          connect.query(
-            "INSERT INTO registros_acciones SET ?",
-            nuevaAccion,
-            (err, result) => {
-              if (err) {
-                connect.rollback(() => {
-                  res.send(err);
-                });
-              } else {
-                // Commit de la transacción
-                connect.commit((err) => {
-                  if (err) {
-                    connect.rollback(() => {
-                      res.send(err);
-                    });
-                  } else {
-                    res.status(200).json({
-                      message: "Creación exitosa",
-                      imageURL1: data.imageURL1,
-                      imagelURL2: data.imagelURL2,
-                    });
-                  }
-                });
+            connect.query(
+              "INSERT INTO registros_acciones SET ?",
+              nuevaAccion,
+              (err, result) => {
+                if (err) {
+                  connect.rollback(() => {
+                    res.send(err);
+                  });
+                } else {
+                  // Actualizar el registro en la tabla de audiencia
+                  connect.query(
+                    "UPDATE audiencia SET ? WHERE id = ?",
+                    [dataToBeChanged, req.params.id],
+                    (err, updateResult) => {
+                      if (err) {
+                        connect.rollback(() => {
+                          res.send(err);
+                        });
+                      } else {
+                        // Commit de la transacción
+                        connect.commit((err) => {
+                          if (err) {
+                            connect.rollback(() => {
+                              res.send(err);
+                            });
+                          } else {
+                            res.send("Actualizado");
+                          }
+                        });
+                      }
+                    }
+                  );
+                }
               }
-            }
-          );
+            );
+          }
         }
-      });
+      );
     });
   });
 };
+
 exports.getAudience = (req, res) => {
   req.getConnection((err, connect) => {
     if (err) return res.send(err);
