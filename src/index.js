9c6f9cd40e3b9332803841f7cce168ed4423e7e4
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-05 18:39:29 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -148,6 +148,29 @@ app.post("/import-csv", (req, res) => {
   });
 });
 
+
+router.post('/procesar', async (req, res) => {
+  try {
+    // Obtenemos el enlace codificado y el correo desde el cuerpo de la solicitud
+    const { enlaceCodificado, email } = req.body;
+
+    // Procesar el enlaceCodificado, por ejemplo, decodificar los datos
+    // ...
+
+    // Enviar el correo al usuario
+    await enviarCorreoUsuario(email, enlaceCodificado);
+
+    // Otras operaciones relacionadas con guardar datos en la base de datos
+    // ...
+
+    res.json({ message: 'Correo enviado y datos procesados correctamente' });
+  } catch (error) {
+    console.error('Error al procesar datos y enviar el correo:', error);
+    res.status(500).json({ error: 'Error al procesar datos y enviar el correo' });
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
