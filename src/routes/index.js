86ebccebf19fe295d7d8dd5be75176ce39ec7e95
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-08 16:15:34 -0300

    test log

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const route = express.Router()
-const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni } = require('../controllers/index')
+const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni, aceptarSub } = require('../controllers/index')
 
 
 route.get('/audience', getAudience)
 route.get('/audience/:dni', getByDni)
 route.post("/add", addAudiencia)
 route.put('/update/:id', editAudience)
+route.put('/acceptSub/:id', aceptarSub)
 route.delete('/delete/:id', deleteAudience)
 
 module.exports = route;
