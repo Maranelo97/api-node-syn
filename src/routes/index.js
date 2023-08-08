bf92584ede3b441d692ffcb9d20ec721b290ab32
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-08 16:21:36 -0300

    ads

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const route = express.Router()
-const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni, aceptarSub } = require('../controllers/index')
+const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni, aceptarSub, rechazarSub } = require('../controllers/index')
 
 
 route.get('/audience', getAudience)
@@ -8,6 +8,7 @@ route.get('/audience/:dni', getByDni)
 route.post("/add", addAudiencia)
 route.put('/update/:id', editAudience)
 route.put('/acceptSub/:id', aceptarSub)
+route.put('reject-sub/:id', rechazarSub)
 route.delete('/delete/:id', deleteAudience)
 
 module.exports = route;
