b85f6c89775b5466276b01002f275a372882e95f
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-09 17:15:26 -0300

    roll

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const route = express.Router()
-const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni, aceptarSub, rechazarSub } = require('../controllers/index')
+const { addAudiencia, getAudience, editAudience, deleteAudience, getByDni, aceptarSub, rechazarSub, rollBackSub } = require('../controllers/index')
 
 
 route.get('/audience', getAudience)
@@ -9,6 +9,7 @@ route.post("/add", addAudiencia)
 route.put('/update/:id', editAudience)
 route.put('/acceptSub/:id', aceptarSub)
 route.put('/reject-sub/:id', rechazarSub)
+route.put('/rollBack/:id', rollBackSub)
 route.delete('/delete/:id', deleteAudience)
 
 module.exports = route;
