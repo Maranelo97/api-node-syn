51471f4efd7249c432a4d4bd194746d88174ace8
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-09 22:20:17 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ const storage = multer.diskStorage({
 });
 
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).single("import-csv");
 
 
 function uploadCsv(uriFile) {
