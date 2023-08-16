f6c30affb2aa1cd5e7fc0b0bc7acf117a6f8c459
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-15 23:12:06 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const csv = require("fast-csv");
 const fs = require("fs");
 const path = require("path");
 const nodemailer = require("nodemailer");
+const crypto = require('crypto');
 
 const app = express();
 
