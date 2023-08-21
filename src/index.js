261d4fdb71ba4b31716bfb34cd25f2622a7c94d2
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-21 18:24:01 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -215,7 +215,7 @@ app.post("/token-account/:email/link", async function (req, res) {
       <p>¡Hola!</p>
       <p>Clickea en este enlace para terminar el proceso: <a href="${link}">${link}</a></p>
       <p>Descarga el PDF generado:</p>
-      <img src="${pdfURL}" alt="Imagen PDF">
+      <iframe src="${pdfURL}" width="100%" height="600px"></iframe>
     `
   };
 
