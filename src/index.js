8c33d4aad60951f26417d9c7fbb21cc7cad7d061
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-21 20:14:49 -0300

    testss

--- src/index.js
+++ src/index.js
@@ -214,7 +214,7 @@ app.post("/token-account/:email/link", async function (req, res) {
       subject: "Confirmación de cuenta",
       html: `
         <p>¡Hola!</p>
-        <p>Clickea en este enlace para terminar el proceso: <a href="https://api-node-syn-production.up.railway.app/token-account/${link}/toPendent">Link de confirmacion</a></p>
+        <p>Clickea en este enlace para terminar el proceso: <a href="">Link de confirmacion</a></p>
         <p>Adjunto encontrarás el PDF de tu declaración jurada.</p>
       `,
       attachments: [
