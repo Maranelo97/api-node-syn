dbf12137e99d0b5349010f6bf43298b6d36f2dba
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-21 22:09:21 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -205,14 +205,14 @@ app.post("/insert-audience", (req, res) => {
       });
     }
 
-    const insertQuery = "INSERT INTO audiencia (name, lastname, province) VALUES (?, ?, ?)";
+    const insertQuery = "INSERT INTO audiencia (name, lastname, province, email) VALUES (?, ?, ?, ?)";
 
     // Usamos Promise.all para asegurarnos de que todas las inserciones se completen antes de responder
     Promise.all(
       audienceData.map(data => {
-        const { name, lastname, province } = data;
+        const { name, lastname, province, email } = data;
         return new Promise((resolve, reject) => {
-          connect.query(insertQuery, [name, lastname, province], (err, result) => {
+          connect.query(insertQuery, [name, lastname, province, email], (err, result) => {
             if (err) {
               console.error("Error al insertar datos en la base de datos:", err);
               reject(err);
