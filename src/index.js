c8701abcaccf473f0ebc3f08157b0f503b16bd4f
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-22 01:11:50 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -289,7 +289,7 @@ app.post("/token-account/:email/link", async function (req, res) {
       subject: "Confirmación de cuenta",
       html: `
         <p>¡Hola!</p>
-        <p>Clickea en este enlace para terminar el proceso: ${link} Al clickear aqui podrás recibir los beneficios de Syngenta Digital Pension</p>
+        <p>Clickea en este enlace para terminar el proceso: https://api-node-syn-production.up.railway.app/token-account/${link}/toPendent Al clickear aqui podrás recibir los beneficios de Syngenta Digital Pension</p>
         <p>Adjunto encontrarás el PDF de tu declaración jurada.</p>
       `,
       attachments: [
