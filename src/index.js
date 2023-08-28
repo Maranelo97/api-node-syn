0a8692227bf808bfffe3b162eefc5e970cd52e24
Mariano Santos <marianoveronsantos@gmail.com>
2023-08-28 13:14:45 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -670,7 +670,7 @@ app.post("/add", (req, res) => {
                       imageURL1: data.imageURL1,
                       imagelURL2: data.imagelURL2,
                     });
-                    io.emit("server:creado")
+                    io.emit("server:creado", data)
                   }
                 });
               }
