a619dfd505638d1fc13dfde3d2c1d5f691225d19
Mariano Santos <marianoveronsantos@gmail.com>
2023-09-24 22:40:56 -0300

    ads to xlsx

--- src/index.js
+++ src/index.js
@@ -246,15 +246,15 @@ app.post("/insert-audience", (req, res) => {
       }
 
       const insertQuery =
-        "INSERT INTO audiencia (name, lastname, province, email, phone, dni, address, address2, dob) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        "INSERT INTO audiencia (name, lastname, province, email, phone, dni, address, address2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
       Promise.all(
         audienceData.map((data) => {
-          const { name, lastname, province, email, phone, dni, address, address2, dob } = data;
+          const { name, lastname, province, email, phone, dni, address, address2 } = data;
           return new Promise((resolve, reject) => {
             connect.query(
               insertQuery,
-              [name, lastname, province, email, phone, dni, address, address2, dob],
+              [name, lastname, province, email, phone, dni, address, address2],
               (err, result) => {
                 if (err) {
                   reject(err);
