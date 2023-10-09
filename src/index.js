dbb267db144717a509e2a1d8a62f6b5db8fe8730
Mariano Santos <marianoveronsantos@gmail.com>
2023-10-08 21:26:40 -0300

    ads

--- src/index.js
+++ src/index.js
@@ -249,38 +249,12 @@ app.post("/insert-audience", (req, res) => {
         "INSERT INTO audiencia (name, lastname, province, email, phone, dni, address, address2, zipCode, location, emailSyngenta, area) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
       Promise.all(
-        records.map((data) => {
-          const {
-            name,
-            lastname,
-            province,
-            email,
-            phone,
-            dni,
-            address,
-            address2,
-            zipCode,
-            location,
-            emailSyngenta,
-            area,
-          } = data;
+        records.map((data) => { // Usar recordsToSend en lugar de audienceData
+          const { name, lastname, province, email, phone, dni, address, address2, zipCode, location, emailSyngenta, area } = data;
           return new Promise((resolve, reject) => {
             connect.query(
               insertQuery,
-              [
-                name,
-                lastname,
-                province,
-                email,
-                phone,
-                dni,
-                address,
-                address2,
-                zipCode,
-                location,
-                emailSyngenta,
-                area,
-              ],
+              [name, lastname, province, email, phone, dni, address, address2, zipCode, location, emailSyngenta, area],
               (err, result) => {
                 if (err) {
                   reject(err);
@@ -309,64 +283,32 @@ app.post("/insert-audience", (req, res) => {
                   });
                 });
               } else {
-                const accionId = 3; // Acción "importó"
-
-                // Obtener el registroId de la importación recién insertada
-                const registroId = result.insertId;
-
-                // Insertar la acción "importó" en la tabla registros_acciones
-                const nuevaAccion = {
-                  accionId,
-                  registroId,
-                  fecha: new Date(),
-                };
-
-                connect.query(
-                  "INSERT INTO registros_acciones SET ?",
-                  nuevaAccion,
-                  (err, actionResult) => {
-                    if (err) {
-                      connect.rollback(() => {
-                        console.error(
-                          "Error al hacer rollback al insertar la acción:",
-                          err
-                        );
-                        res.status(500).json({
-                          ok: false,
-                          message: "Error al insertar la acción 'importó'",
-                          error: err.message,
-                        });
+                connect.commit((commitErr) => {
+                  if (commitErr) {
+                    connect.rollback(() => {
+                      console.error(
+                        "Error al hacer commit de la transacción:",
+                        commitErr
+                      );
+                      res.status(500).json({
+                        ok: false,
+                        message: "Error al hacer commit de la transacción",
+                        error: commitErr.message,
                       });
-                    } else {
-                      connect.commit((commitErr) => {
-                        if (commitErr) {
-                          connect.rollback(() => {
-                            console.error(
-                              "Error al hacer commit de la transacción:",
-                              commitErr
-                            );
-                            res.status(500).json({
-                              ok: false,
-                              message: "Error al hacer commit de la transacción",
-                              error: commitErr.message,
-                            });
-                          });
-                        } else {
-                          res.status(200).json({
-                            ok: true,
-                            message:
-                              "Datos insertados correctamente en la tabla de audiencia y se registró la importación",
-                            importName,
-                          });
-                          io.emit("server:audienceInserted", {
-                            importName,
-                            importedRows: records.length,
-                          });
-                        }
-                      });
-                    }
+                    });
+                  } else {
+                    res.status(200).json({
+                      ok: true,
+                      message:
+                        "Datos insertados correctamente en la tabla de audiencia y se registró la importación",
+                      importName
+                    });
+                    io.emit("server:audienceInserted", {
+                      importName,
+                      importedRows: recordsToSend.length, // Usar recordsToSend en lugar de audienceData
+                    });
                   }
-                );
+                });
               }
             }
           );
@@ -387,8 +329,6 @@ app.post("/insert-audience", (req, res) => {
     });
   });
 });
-
-
 
 
 //Envio y Enlace de Validación Post Formulario
