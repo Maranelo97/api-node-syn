73f5f37fc95f308d88ab9b1f473f9a328fd303cb
Mariano Santos <marianoveronsantos@gmail.com>
2023-10-16 00:44:46 -0300

    test

--- src/index.js
+++ src/index.js
@@ -290,6 +290,7 @@ app.post("/insert-audience", (req, res) => {
                   const nuevaAccion = {
                     accionId,
                     registroId,
+                    importName, // Agrega el importName como parte de la acción
                     fecha: new Date(),
                   };
 
