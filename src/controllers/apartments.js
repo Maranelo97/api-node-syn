39c320c31b12ac6cc033166e13c2883dc4a038eb
Mariano Santos <marianoveronsantos@gmail.com>
2023-11-20 22:35:09 -0300

    ads

--- src/controllers/apartments.js
+++ src/controllers/apartments.js
@@ -9,3 +9,68 @@ exports.getAllApartments = (req, res) => {
         });
     });
 };
+
+exports.createApartment = (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        const apartmentData = {
+            NombreDepartamento: req.body.NombreDepartamento,
+            CantidadEmpleados: 0
+        };
+
+        conn.query('INSERT INTO Departamentos SET ?', [apartmentData], (err, result) => {
+            if (err) return res.send(err);
+
+            res.send('Departamento creado exitosamente');
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.status(500).json({ error: 'Error interno del servidor' });
+
+        const { id } = req.params; 
+        const { NombreDepartamento } = req.body;
+
+        const updateUserQuery = `
+            UPDATE Departamentos 
+            SET 
+                NombreDepartamento = ?
+            WHERE AreaID = ?`;
+
+        conn.query(
+            updateUserQuery,
+            [NombreDepartamento, id],
+            (err, result) => {
+                if (err) {
+                    console.error('Error al actualizar los datos:', err);
+                    return res.status(500).json({ error: 'Error interno del servidor' });
+                }
+
+                if (result.affectedRows > 0) {
+                    res.json({ message: 'Datos actualizados exitosamente' });
+                } else {
+                    res.status(404).json({ error: 'Usuario no encontrado' });
+                }
+            }
+        );
+    });
+};
+
+exports.deleteDepartment = (req, res) => {
+    req.getConnection((err, connect) => {
+      if (err) return res.send(err);
+  
+      connect.query(
+        "DELETE FROM Departamentos WHERE AreaID = ?",
+        [req.params.id],
+        (err, result) => {
+          if (err) return res.send(err);
+  
+          res.send("Eliminado con exito");
+        }
+      );
+    });
+  };
--- src/routes/apartmentsRoutes.js
+++ src/routes/apartmentsRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const apartmentRoutes = express.Router();
 
-const { getAllApartments } = require('../controllers/apartments');
+const { getAllApartments, deleteDepartment, createApartment, update } = require('../controllers/apartments');
 
 apartmentRoutes.get('/apartments', getAllApartments);
+apartmentRoutes.post("/apartments/create", createApartment);
+apartmentRoutes.put("/updateDepartment/:id", update)
+apartmentRoutes.delete("/deleteApartment/:id", deleteDepartment);
 
 module.exports = apartmentRoutes;
