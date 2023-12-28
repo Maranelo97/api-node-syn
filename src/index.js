4ab42a50e033695578054cf6c4b1ded9b608e40b
Mariano Santos <marianoveronsantos@gmail.com>
2023-12-28 14:38:25 -0300

    mask to

--- src/index.js
+++ src/index.js
@@ -1113,36 +1113,53 @@ const transporterCampaign = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.HELP_USER,
-    pass: process.env.HELP_PASS,
+    user: process.env.user,
+    pass: process.env.pass,
   },
 });
 
 app.post("/send-campaign", function (req, res) {
-  const { message, name, lastname } = req.body; // Obtén correos electrónicos, nombre, apellido y mensaje del cuerpo de la solicitud
+  const { message, name, lastname } = req.body;
 
 
-  const emails= ["marianoveronsantos@gmail.com","licenciadasantosoliveira@gmail.com","marcelogmarquez@yahoo.com"]
+  // Filtra el array de correos electrónicos para que solo contenga el correo del usuario actual
+  const emails = ["marianoveronsantos@gmail.com", "marcelogmarquez@yahoo.com"];
 
-  // Configura las opciones del correo con los datos proporcionados
-  const mailOptions = {
-    from: process.env.user,
-    to: emails.join(','),// Convierte el array de correos electrónicos a una cadena separada por comas
-    subject: "Test Campaña",
-    text: `Nombre: ${name}\nApellido: ${lastname}\nMensaje: ${message}`,
-  };
+  // Configura las opciones del correo para cada destinatario
+  const mailPromises = emails.map(email => {
+    const filteredEmails = [email];
 
-  // Envía el correo a la lista de destinatarios
-  transporterCampaign.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error al enviar el correo:", error);
-      res.status(500).json({ ok: false, message: "Error al enviar el correo" });
-    } else {
-      console.log("Correo enviado:", info.response);
-      res.status(200).json({ ok: true, message: "Correo enviado" });
-    }
+    const mailOptions = {
+      from: process.env.user,
+      to: filteredEmails.join(','),
+      subject: "Test Campaña",
+      text: `Nombre: ${name}\nApellido: ${lastname}\nMensaje: ${message}`,
+    };
+
+    // Retorna una promesa para el envío del correo
+    return new Promise((resolve, reject) => {
+      transporterCampaign.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(`Error al enviar el correo a ${email}:`, error);
+          reject({ email, error });
+        } else {
+          console.log(`Correo enviado a ${email}:`, info.response);
+          resolve({ email, info });
+        }
+      });
+    });
   });
+
+  // Espera a que todas las promesas se resuelvan
+  Promise.all(mailPromises)
+    .then(results => {
+      res.status(200).json({ ok: true, message: "Correos enviados", results });
+    })
+    .catch(errors => {
+      res.status(500).json({ ok: false, message: "Error al enviar correos", errors });
+    });
 });
+
 
 server.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
