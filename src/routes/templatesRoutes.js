75f38e95f789a73f0dbe1b55596cc4f35f021676
Mariano Santos <marianoveronsantos@gmail.com>
2024-02-09 09:59:27 -0300

    create template controller added

--- src/routes/templatesRoutes.js
+++ src/routes/templatesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const templateRouter = express.Router();
 
-const { getAllTemplates } = require('../controllers/templatesControllers')
+const { getAllTemplates, createTemplate } = require('../controllers/templatesControllers')
 
 const allowOnlyFromSpecificOrigin = (req, res, next) => {
     const allowedOrigins = ['http://localhost:3000', 'https://admin.pensionplan.com.ar', 'https://pensionplan.com.ar'];
@@ -19,5 +19,6 @@ const allowOnlyFromSpecificOrigin = (req, res, next) => {
   templateRouter.use(allowOnlyFromSpecificOrigin)
 
 templateRouter.get('/getAllTemplates', getAllTemplates);
+templateRouter.post('/createTemplate', createTemplate);
 
 module.exports = templateRouter;
