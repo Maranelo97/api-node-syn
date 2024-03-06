af6c3e70d6ce9aa2a9c337336e5ff2c02b054159
Mariano Santos <marianoveronsantos@gmail.com>
2024-03-06 20:48:24 -0300

    ads

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,15 +24,6 @@ const {
 const { getImports, deleteImport } = require("../controllers/imports");
 const { getHealth } = require("../controllers/health-controller");
 
-const allowOnlyFromSpecificOrigin = (req, res, next) => {
-const allowedOrigins = ['http://localhost:3000', 'https://admin.pensionplan.com.ar', 'https://pensionplan.com.ar','http://localhost:5173','https://api.pensionplan.com.ar'];
-const origin = req.headers.origin;
-if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      return next();
-  }
-return res.status(403).json({ error: 'Acceso no permitido desde esta dirección.' });
-};
 
 
 route.use(allowOnlyFromSpecificOrigin)
